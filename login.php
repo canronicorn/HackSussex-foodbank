ecc516bdd43b797cfb91f90b68ca66fe825592f0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2023-02-19 09:16:49 +0000

    added the other functions

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required maxlength="20">
 
-            <button onclick="send_login_request('uname','psw')" type="submit">Login</button>
+            <button onclick="send_login_request()" type="submit">Login</button>
           </div>
           <p>Don't have an account? <a href="signup.php">Sign up</a></p>
           <button><a href="index.php">Back</a></button>
@@ -63,4 +63,67 @@ function login($username, $password) {
       echo "\033[31mWe have an error in login\033[0m\n";
       echo "\033[31m" . $error->getMessage() . "\033[0m\n";
     }
+  }
+
+  function createPostRequest($collection, $database, $dataSource, $filter) {
+    $url = 'https://eu-west-2.aws.data.mongodb-api.com/app/data-wugsm/endpoint/data/v1/action/findOne';
+    $data = array(
+      'collection' => $collection,
+      'database' => $database,
+      'dataSource' => $dataSource,
+      'filter' => $filter
+    );
+    $headers = array(
+      'Content-Type: application/json',
+      'Access-Control-Request-Headers: *',
+      'api-key: KrXM0dT8X5oZLZRqvKu69knfpZzF4ouo9WDug2HxpAHJ5Z7eoNSVJCVsCpkWXYz9'
+    );
+  
+    $options = array(
+      'http' => array(
+        'header' => $headers,
+        'method' => 'POST',
+        'content' => json_encode($data),
+      ),
+    );
+    $context  = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+    return json_decode($response, true);
+  }
+  
+  function grabUserInfo($userId) {
+    $data = createPostRequest('userInfo', 'Hack', 'hacksussexDB', array('_id' => array('$oid' => $userId)));
+    return $data;
+  }
+  
+  function grabinventoryItems($userId) {
+    $url = 'https://eu-west-2.aws.data.mongodb-api.com/app/data-wugsm/endpoint/data/v1/action/find';
+    $data = array(
+      'collection' => 'inventory',
+      'database' => 'Hack',
+      'dataSource' => 'hacksussexDB',
+      'filter' => array('userInfo.0._id' => array('$oid' => $userId))
+    );
+    $headers = array(
+      'Content-Type: application/json',
+      'Access-Control-Request-Headers: *',
+      'api-key: KrXM0dT8X5oZLZRqvKu69knfpZzF4ouo9WDug2HxpAHJ5Z7eoNSVJCVsCpkWXYz9'
+    );
+  
+    $options = array(
+      'http' => array(
+        'header' => $headers,
+        'method' => 'POST',
+        'content' => json_encode($data),
+      ),
+    );
+    $context  = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+    $data = json_decode($response, true);
+    $count = 0;
+    foreach ($data['documents'] as $item) {
+      print_r($item);
+      $count++;
+    }
+    print_r($count);
   }
