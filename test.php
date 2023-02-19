08789ab22bb9147e97a45187f20fe78652a1387b
Flynnonym <flynnjack728@gmail.com>
2023-02-19 06:57:11 +0000

    Update test.php

--- test.php
+++ test.php
@@ -18,6 +18,22 @@
         <th>Coding Score</th>
         <th>GFG Articles</th>
     </tr>
+    <!-- PHP CODE TO FETCH DATA FROM ROWS -->
+    <?php
+        // LOOP TILL END OF DATA
+        while($rows=$result->fetch_assoc())
+        {
+    ?>
+    <tr>
+        <!-- FETCHING DATA FROM EACH ROW OF EVERY COLUMN -->
+        <td><?php echo $rows['username'];?></td>
+        <td><?php echo $rows['problems'];?></td>
+        <td><?php echo $rows['score'];?></td>
+        <td><?php echo $rows['articles'];?></td>
+    </tr>
+    <?php
+        }
+    ?>
 </table>
 
 </body>
