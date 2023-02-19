935582ce6039bfb1b84876652459e533afb96614
canronicorn <5000rasmus@gmail.com>
2023-02-19 06:44:33 +0000

    Changed test.hph file with Azure suggested file.

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:hacksussex-foodbank-sql.database.windows.net,1433; Database = hacksussex-foodbank-sqldatabase", "foodBankAdmin", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:hacksussex-foodbank-sql.database.windows.net,1433; Database = hacksussex-foodbank-sqldatabase", "foodBankAdmin", "YHcRKdUx8Z8GFsTng9sA");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "foodBankAdmin", "pwd" => "{your_password_here}", "Database" => "hacksussex-foodbank-sqldatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "foodBankAdmin", "pwd" => "YHcRKdUx8Z8GFsTng9sA", "Database" => "hacksussex-foodbank-sqldatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:hacksussex-foodbank-sql.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
